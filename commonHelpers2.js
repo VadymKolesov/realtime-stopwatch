1d0a7aaa62b04facc2ef825bc95b055eb3d891e4
VadymKolesov <46047631+VadymKolesov@users.noreply.github.com>
2024-03-20 19:38:19 +0000

    Deploying to gh-pages from @ VadymKolesov/realtime-stopwatch@7922a6143c58ace16c0069136f37b58e843f8325 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{c as r,r as e,l as t}from"./assets/refs-c790447f.js";import"./assets/vendor-451a0b16.js";r();e.logOutBtn.addEventListener("click",t);e.navName.innerHTML=JSON.parse(localStorage.getItem("log")).user;
+import{c as r,r as e,l as t}from"./assets/refs-0b29b03b.js";import"./assets/vendor-451a0b16.js";r();e.logOutBtn.addEventListener("click",t);e.navName.innerHTML=JSON.parse(localStorage.getItem("log")).user;
 //# sourceMappingURL=commonHelpers2.js.map
