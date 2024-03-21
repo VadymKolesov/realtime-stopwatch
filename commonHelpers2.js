a6e8ff1d9e3ff7ac661e7caccf82354598084366
VadymKolesov <46047631+VadymKolesov@users.noreply.github.com>
2024-03-21 09:34:01 +0000

    Deploying to gh-pages from @ VadymKolesov/realtime-stopwatch@c6e7fee4222be3b64d2bd95863e3e9a40a724cdf 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{c as r,r as e,l as t}from"./assets/refs-bd35c26c.js";import"./assets/vendor-451a0b16.js";r();e.logOutBtn.addEventListener("click",t);e.navName.innerHTML=JSON.parse(localStorage.getItem("log")).user;
+import{a,c as t,r as e,l as s}from"./assets/firebase-api-fdd5c21b.js";import"./assets/vendor-451a0b16.js";a("default_stopwatch_1");t();e.logOutBtn.addEventListener("click",s);e.navName.innerHTML=JSON.parse(localStorage.getItem("log")).user;
 //# sourceMappingURL=commonHelpers2.js.map
